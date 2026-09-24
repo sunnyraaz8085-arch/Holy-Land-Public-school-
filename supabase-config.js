04ba5ddacea263aa415e0ac764a9785f5ce46ed4
sunnyraaz8085-arch <sunnyraaz8085@gmail.com>
2026-09-24 20:34:12 +0530

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
-const SUPABASE_URL = https://rxrundokeksglyigbocv.supabase.co/rest/v1/
+const SUPABASE_URL = "https://rxrundokeksglyigbocv.supabase.co";
 
-const SUPABASE_PUBLISHABLE_KEY =sb_publishable_-wkRDWu6FloTLy5xsrvvwQ_Qt3sJkfb
+const SUPABASE_PUBLISHABLE_KEY = sb_publishable_-wkRDWu6FloTLy5xsrvvwQ_Qt3sJkfb
 
 const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
