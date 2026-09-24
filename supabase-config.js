ff908d8a83447e566f1c63ca6de02da5973e7109
sunnyraaz8085-arch <sunnyraaz8085@gmail.com>
2026-09-24 16:05:23 +0530

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "यहाँ अपना Supabase API URL डालें"; https://rxrundokeksglyigbocv.supabase.co/rest/v1/
+const SUPABASE_PUBLISHABLE_KEY = "यहाँ अपना Publishable Key डालें";sb_publishable_-wkRDWu6FloTLy5xsrvvwQ_Qt3sJkfb
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
